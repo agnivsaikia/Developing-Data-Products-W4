17748f636312e186dd9ec84afcab6fe44f4d2c70
agnivsaikia <37177994+agnivsaikia@users.noreply.github.com>
2020-11-04 12:59:46 +0530

    Add files via upload

---
+++ Shiny Application/Shiny Application/ui.R
@@ -0,0 +1,72 @@
+library(shiny)
+
+shinyUI(
+  navbarPage("Shiny Application",
+             tabPanel("Analysis",
+                      fluidPage(
+                        titlePanel("The relationship between variables and mileage(MPG)"),
+                        sidebarLayout(
+                          sidebarPanel(
+                            selectInput("variable", "Variable:",
+                                        c("Transmission" = "am",
+                                          "Weight (lb/1000)" = "wt",
+                                          "Rear axle ratio" = "drat",
+                                          "Number of cylinders" = "cyl",
+                                          "Displacement (cu.in.)" = "disp",
+                                          "Gross horsepower" = "hp",
+                                          "1/4 mile time" = "qsec",
+                                          "Engine shape" = "vs",
+                                          "Number of forward gears" = "gear",
+                                          "Number of carburetors" = "carb"
+                                        )),
+                            
+                            checkboxInput("outliers", "Show BoxPlot's outliers", FALSE)
+                          ),
+                          
+                          mainPanel(
+                            h3(textOutput("caption")),
+                            
+                            tabsetPanel(type = "tabs", 
+                                        tabPanel("BoxPlot", plotOutput("mpgBoxPlot")),
+                                        tabPanel("Regression model", 
+                                                 plotOutput("mpgPlot"),
+                                                 verbatimTextOutput("fit")
+                                        )
+                            )
+                          )
+                        )
+                      )
+             ),
+             tabPanel("Data Detail",
+                      h2("Motor Trend Car Road Tests"),
+                      hr(),
+                      h3("Description"),
+                      helpText("The data was extracted from the 1974 Motor Trend US magazine,",
+                               " and comprises fuel consumption and 10 aspects of automobile design and performance",
+                               " for 32 automobiles (1973-74 models)."),
+                      h3("Format"),
+                      p("A data frame with 32 observations on 11 variables."),
+                      
+                      p("  [, 1]   mpg         Miles/(US) gallon"),
+                      p("  [, 2]	 cyl	 Number of cylinders"),
+                      p("  [, 3]	 disp	 Displacement (cu.in.)"),
+                      p("  [, 4]	 hp	 Gross horsepower"),
+                      p("  [, 5]	 drat	 Rear axle ratio"),
+                      p("  [, 6]	 wt	 Weight (lb/1000)"),
+                      p("  [, 7]	 qsec	 1/4 mile time"),
+                      p("  [, 8]	 vs	 Engine(V-shaped or Straight)"),
+                      p("  [, 9]	 am	 Transmission (0 = automatic, 1 = manual)"),
+                      p("  [,10]	 gear	 Number of forward gears"),
+                      p("  [,11]	 carb	 Number of carburetors"),
+                      
+                      h3("Source"),
+                      
+                      p("Henderson and Velleman (1981), Building multiple regression models interactively. Biometrics, 37, 391-411.")
+             ),
+             tabPanel("Go back to Github",
+                      a("https://github.com/agnivsaikia/Developing-Data-Products-W4"),
+                      hr(),
+                      h4("The name of the repository is Developing Data Products W4")
+             )
+  )
+)
